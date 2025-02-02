27b01b01f6f73a3f54cf0e14aeae121d1d421472
Arshpreet Singh <arshsomal100@gmail.com>
2025-02-02 17:53:45 +0530

    Final checking

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1 className="text-3xl text-white">Arsh Somal</h1>
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-3xl text-white font-bold">Arsh Somal</h1>
       
     </div>
   );
